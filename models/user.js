8ca232e50325b52f2dc6e6e663f56725967fdc6e
Amistad Alex (ERNI) <amal@erni.ph>
2018-10-21 20:55:12 +0800

    added error log

--- models/user.js
+++ models/user.js
@@ -45,9 +45,17 @@ UserSchema.methods.generateAuthToken = function () {
 
 UserSchema.methods.download = function(uri, filename, callback){
     request.head(uri, function(err, res, body){
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        if (err) {
+            fs.writeFile("/tmp/errors.txt", err, function(error) {
+                if(error) {
+                    return console.log(error);
+                }
+                console.log("The file was saved!");
+            }); 
+        }
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   };
 
